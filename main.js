3142ee0c0ddda6df2ef11f0dbc0001016547fca0
smikerin <91702021+smikerin@users.noreply.github.com>
2022-01-12 23:28:37 -0800

    fixed index

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-//alert('test');
+ //alert(test);
+
 //https://tinyurl.com/dynamic-html-checker
 document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+
 document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);  
 
- //manages mobile nav 
- function myFunction() {
- var x = document.getElementById("myTopnav");
- if (x.className === "topnav") {
- x.className += " responsive";
- } else {
- x.className = "topnav";
- }
- } 
+//manages mobile nav 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}   
+
+let myDate = new Date();
+let myYear = myDate.getFullYear();
+document.getElementById(
